d2982fb927a97238e6174043086990e7ce9a5777
Ryuya <ryuyxx@gmail.com>
2023-09-10 12:54:25 +0900

    📚 Update Documents (#30)

--- website/theme.config.jsx
+++ website/theme.config.jsx
@@ -12,15 +12,33 @@ export default {
   project: {
     link: 'https://github.com/3ru/gpt-translate',
   },
+  head: (
+    <>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta
+        name='og:title'
+        content='GPT-Translate: Automate translation workflow using AI'
+      />
+      <meta
+        name='description'
+        content='GPT-Translate: Automate translation workflow using AI'
+      />
+    </>
+  ),
   footer: {
     text: (
       <span>
         MIT {new Date().getFullYear()} ©{' '}
-        <a href="https://github.com/3ru" target="_blank">
+        <a href='https://github.com/3ru' target='_blank'>
           Ryuya
         </a>
         .
       </span>
     ),
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – GPT-Translate',
+    }
   },
 }
